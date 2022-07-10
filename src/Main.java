7ea6ba75cbd723da1ff81f9638db6bb62d9b4dd2
cedoq <cedoq2@gmail.com>
2022-07-10 21:38:00 +0200

    1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static BufferedReader bR = new BufferedReader(new InputStreamReader(System.in))   ;
+    static BufferedReader bR = new BufferedReader(new InputStreamReader(System.in))
+            ;
     public static void main(String[] args) throws IOException {
         System.out.println("1 = первое задание, 2= второе задание ");
         int a = Integer.parseInt(bR.readLine());
@@ -12,31 +13,31 @@ public class Main {
             int i;
             for(i = 0; i<=100;i++){
                 String k = String.valueOf(i);
-                if (!k.contains("4")) {
+                if (!k.contains("4") && !k.contains("9") ) {
                     System.out.println(i);
                 }
             }
-            }
-        else if(a==2){
-            int i = 1; //day count
-            int k = 0; //money per day
-            int sum=1; //total money
+            } else if (a==2) {
+
+
+        int i = 2; //day count
+            int k = 1; //money per day
+            int sum=1;//total money
+            System.out.println("1 день -> " + sum);
             while(sum<1000000){
-                if(k==0){
-                    k = i*3;
-                }
-                else{
-                    k = k*3;
-                }
+                k = k*3;
                 if(sum+k>1000000){break;}
+
                 sum = sum + k;
                 System.out.println(i + " день -> "+ sum);
                 i++;
 
             }
+
+
         }
         else{
             System.out.println("ярик ну ты тупой чтоль?");
         }
         }
-    }
+}
